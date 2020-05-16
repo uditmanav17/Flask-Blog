f0251b090daa303e3f5e372bca16a67f9733e5b0
Udit Manav <uditmanav17@gmail.com>
2020-05-16 22:43:20 +0530

    added sample queries, 'password' spell correction

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -13,8 +13,8 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     img_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    passowrd = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy="dynamic")
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True) # lazy=True
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.img_file}')"
