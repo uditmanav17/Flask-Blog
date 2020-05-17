191b25fe569840706264e6b50911b0e50a8d40f5
Udit Manav <uditmanav17@gmail.com>
2020-05-17 16:42:07 +0530

    Restructured project to package; added user authentication

---
+++ Flask_blog/flaskblog/forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', 
+                            validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', 
+                         validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', 
+                                      validators=[DataRequired(), EqualTo('password', message='Passwords must match!')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already taken, please choose another.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(username=email.data).first()
+        if user:
+            raise ValidationError('Email already taken, please choose another.')
+
+
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', 
+                         validators=[DataRequired(), Email()]) 
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+    
+
+
+
