fac5cc7c02186fda0599d7b2c8cd8a172935e4ce
Udit Manav <uditmanav17@gmail.com>
2020-05-29 13:20:55 +0530

    added custom error pages

--- Flask_blog/flaskblog/__init__.py
+++ Flask_blog/flaskblog/__init__.py
@@ -28,10 +28,12 @@ def create_app(config_class=Config):
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
 
     # register blueprints to our app
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
