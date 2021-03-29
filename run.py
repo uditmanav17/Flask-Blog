c8f40cfb864168b5ef6da801b00638d4b51cd579
Udit Manav <uditmanav17@gmail.com>
2021-03-29 14:22:21 +0530

    updated run.py with heroku commpatibility

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from flaskblog import create_app
+import os
+
 
 app = create_app()
-
+port = os.environ.get('PORT') or 5000
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=port)
